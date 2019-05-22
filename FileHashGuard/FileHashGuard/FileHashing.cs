5e918f6411020a615656303efe1399fe0c2fb09c
Vallejo Juan Marcos <oslboreal@gmail.com>
2019-05-22 15:50:27 -0300

    FileHashing

---
+++ FileHashGuard/FileHashGuard/FileHashing.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace FileHashGuard
+{
+    internal enum EncryptionMethod
+    {
+        SHA256,
+        MD5
+    }
+    public static class FileHashing
+    {
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="path"></param>
+
+
+        private static string _hash;
+        private static string _path;
+
+        public static string Hash { get => _path; }
+        public static string Path { get => _path; }
+
+        /// <summary>
+        /// Generates a new hash.
+        /// </summary>
+        /// <param name="filePath">To encrypt file path.</param>
+        /// <param name="encryptionMethod">Encryption method</param>
+        /// <returns></returns>
+        private static string EncodeFile(string filePath, EncryptionMethod encryptionMethod)
+        {
+            try
+            {
+                switch (encryptionMethod)
+                {
+                    case EncryptionMethod.SHA256:
+
+                        using (SHA256 Sha256 = SHA256.Create())
+                        using (FileStream stream = File.OpenRead(Path))
+                            _hash = Sha256.ComputeHash(stream).ToString();
+
+                        break;
+                    case EncryptionMethod.MD5:
+
+                        using (var md5 = MD5.Create())
+                        using (var stream = File.OpenRead(Path))
+                            _hash = md5.ComputeHash(stream).ToString();
+
+                        break;
+                }
+                return _hash;
+            }
+            catch (Exception)
+            {
+                throw new Exception("Error encrypting the specified file.");
+            }
+        }
+    }
+}
